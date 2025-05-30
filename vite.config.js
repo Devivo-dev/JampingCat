be8d3db10c54e60d24f6d82c17f285538c571194
MrForestino <devivo.dev@gmail.com>
2025-05-30 18:10:35 +0200

    UPDATE

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,15 @@
 import { defineConfig } from 'vite';
-import path from 'path';
-import { globSync } from 'glob'; // <- виправлено
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: '/JampingCat/',
   root: 'src',
   build: {
     rollupOptions: {
-      input: globSync('./src/**/*.html') // <- тепер правильно
+      input: glob.sync('./src/*.html'),
     },
     outDir: '../dist',
-    emptyOutDir: true
   },
-  resolve: {
-    alias: {
-      '@scss': path.resolve(__dirname, './src/scss'),
-      '@scripts': path.resolve(__dirname, './src/scripts'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-    }
-  },
-  plugins: [
-    injectHTML(),
-    FullReload(['./src/**/*.html'])
-  ]
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
